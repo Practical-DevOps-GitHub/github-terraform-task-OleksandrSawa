53863bdcfc69f6e17ed79247e01055a7be32bd73
Oleksandr <Sawokpatriot@gmail.com>
2023-08-03 23:47:16 +0200

    123

--- main.tf
+++ main.tf
@@ -1,64 +1,70 @@
 # Настройка провайдера GitHub
 provider "github" {
-  token = "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"
+  token = "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"
 }
-
+resource "github_repository" "repo" {
+  name   = "github-terraform-task-OleksandrSawa"
+}
+# Ресурс для установки ветки по умолчанию
+resource "github_branch_default" "default_branch" {
+  repository = github_repository.repo.name
+  branch     = "main"
+}
 # Ресурс для добавления соавтора
 resource "github_repository_collaborator" "softservedata_collaborator" {
-  repository = "github-terraform-task-OleksandrSawa"
+  repository = github_repository.repo.name
   username   = "softservedata"
   permission = "push" # или "admin", в зависимости от требуемых прав доступа
 }
 
 # Ресурс для настройки защиты ветки main
 resource "github_branch_protection" "main_protection" {
-  repository = "github-terraform-task-OleksandrSawa"
-  branch     = "main"
-
+  repository_id = github_repository.repo.id
+  pattern    = "main"
   enforce_admins = true
 }
 
 # Ресурс для настройки защиты ветки develop
 resource "github_branch_protection" "develop_protection" {
-  repository = "github-terraform-task-OleksandrSawa"
-  branch     = "develop"
+  repository_id = github_repository.repo.id
+  pattern       = "develop"
 
   required_pull_request_reviews {
-    dismissal_restrictions {
-      users = ["OleksandrSawa"]
-    }
-    dismiss_stale_reviews = false
-    require_code_owner_reviews = true
+    dismiss_stale_reviews           = false
+    require_code_owner_reviews      = true
     required_approving_review_count = 2
   }
 
   enforce_admins = true
 }
 
-# Ресурс для назначения владельца кода для файлов в ветке main
-resource "github_branch_default_branch" "main_default_branch" {
-  repository = "github-terraform-task-OleksandrSawa"
-  branch     = "main"
-}
 
 # Ресурс для добавления шаблона запроса на слияние
 resource "github_repository_file" "pull_request_template" {
-  repository = "github-terraform-task-OleksandrSawa"
-  file_path  = ".github/pull_request_template.md"
-  content    = file("pull_request_template.md")
+  repository = github_repository.repo.id
+  file       = ".github/pull_request_template.md"
+  content    = <<-EOT
+## Describe your changes
+
+## Issue ticket number and link
+
+## Checklist before requesting a review
+- [ ] I have performed a self-review of my code
+- [ ] If it is a core feature, I have added thorough tests
+- [ ] Do we need to implement analytics?
+- [ ] Will this be part of a product update? If yes, please write one phrase about this update
+  EOT
 }
 
-# Ресурс для добавления deploy key
+#rесурс для добавления deploy key
 resource "github_repository_deploy_key" "deploy_key" {
-  repository = "github-terraform-task-OleksandrSawa"
+  repository = github_repository.repo.name
   title      = "DEPLOY_KEY"
   key        = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIBTcJlWp/yumhp1VVzaB4KV1RcEDv0vrgPLMSalEdVXy sawok@DESKTOP-TAGIO1T"
 }
-
 # Ресурс для создания Discord webhook
 resource "github_repository_webhook" "discord_webhook" {
-  repository = "github-terraform-task-OleksandrSawa"
-  name       = "Discord"
+  repository = github_repository.repo.id
   events     = ["pull_request"]
   configuration {
     url = "https://discord.com/api/webhooks/1136733262912438312/XWgFaVF7QRqfcVxcXisRepJkhZnI4_CGCrr5rNc0_jffICkuyjP2PbvONp9Vhdi64n4a/github"
